2b3dbaa278100bdca21b41fa621a0ec386eba87c
khomyakeugene <khomyak_eugene@yahoo.com>
2016-04-25 22:13:10 +0300

    JEE Unit 5.2.6 StudyCase

--- src/main/java/com/company/tasks/LogAspect.java
+++ src/main/java/com/company/tasks/LogAspect.java
@@ -1,13 +1,15 @@
 package com.company.tasks;
 
-import org.aspectj
+import org.aspectj.lang.ProceedingJoinPoint;
 
 /**
  * Created by Yevhen on 25.04.2016.
  */
 public class LogAspect {
 
-    public void onExecute(ProcessingJointPoint ) {
-
+    public void onExecute(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("LogAspect: Before execution of: " + pjp.getSignature().getName());
+        pjp.proceed();
+        System.out.println("LogAspect: After execution of: " + pjp.getSignature().getName());
     }
 }
